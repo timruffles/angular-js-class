49be2a83c71503e5f7b9c379b9957926c671e94d
Tim Ruffles <timruffles@googlemail.com>
2014-02-18 16:03:31 +0000

    nicer routes docs

--- exercises/routes/main.js
+++ exercises/routes/main.js
@@ -2,11 +2,12 @@ var app = angular.module("exercise",["ngRoute"]);
 
 app.config(function($locationProvider) {
   // won't work for file://
+  // normally we'd use this to enable pushState
   // $locationProvider.html5Mode(true);
 });
 
 app.config(function($routeProvider) {
-  // we'd like to define 3 routes for the /, b and c paths
+  // we'd like to define three routes for the three paths
   // - how can we do that?
 });
 
