c6143bd2055308c47ad7be85d5d4305604074efb
Tim Ruffles <oi@truffles.me.uk>
2014-10-01 21:12:38 +0100

    ex

--- exercises/controllers-scopes/main.js
+++ exercises/controllers-scopes/main.js
@@ -3,14 +3,27 @@ var app = angular.module("exercise",[]);
 // YOUR CODE
 // we're missing a controller
 // - you'll see an error in the console that'll suggest what to fix.
+app.controller("defineMe",function($scope) {
+})
 
 app.controller("listful",function($scope) {
   // YOUR CODE
-  $scope.list = []; // looking a bit bare
+  $scope.list = [{
+    title: "mango",
+  }]; // looking a bit bare
+  $scope.remove = function(index) {
+    $scope.list.splice(index, 1);
+  }
 })
 
 app.controller("tabs",function($scope) {
   // YOUR CODE
   // the view is expecting a function to be defined
   // on the scope - can you fill it in?
+  $scope.tab = "a";
+
+
+  $scope.showTab = function(tab) {
+    $scope.tab = tab;
+  };
 })
--- exercises/tricky-scopes/main.js
+++ exercises/tricky-scopes/main.js
@@ -1,16 +1,16 @@
 var app = angular.module("exercise",[]);
 
 app.controller("main",function($scope) {
-  $scope.count = 0
+  $scope.counter = {count: 0};
 })
 
-app.controller("child",function($scope) {
+app.controller("child",function($scope, $timeout) {
   $scope.increment = function() {
-    $scope.count += 1
+    $scope.counter.count += 1;
   }
   $scope.delayedIncrement = function() {
-    setTimeout(function() {
-      $scope.count += 1
+    $timeout(function() {
+      $scope.counter.count += 1;
     },250); // just perceptible delay
   }
 })
