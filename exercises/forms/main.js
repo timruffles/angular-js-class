aeda29ceb76498ebb8fcbc922053c7c79ea66dcc
Tim Ruffles <oi@truffles.me.uk>
2014-10-27 11:33:36 +0000

    answered

--- exercises/forms/main.js
+++ exercises/forms/main.js
@@ -3,9 +3,12 @@ var exercise = angular.module("exercise",[])
 exercise.controller("BuyShoelaceCtrl", function($scope) {
   $scope.buy = function() {
     // TODO don't run if form is invalid
+    if($scope.shoelaceForm.$invalid) return;
     // TODO add lace to laces list
+    $scope.laces.push($scope.shoelace);
     // TODO reset form
-  } 
+    $scope.shoelace = {};
+  }
 });
 
 exercise.controller("ShoelaceListCtrl", function($scope) {
