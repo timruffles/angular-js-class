00a410f361b7f154a803c6b65b24f0e182524b77
Tim Ruffles <oi@truffles.me.uk>
2014-10-27 23:19:14 +0000

    complete example

--- exercises/complete-example/main.js
+++ exercises/complete-example/main.js
@@ -2,6 +2,8 @@ angular.module("exercise",["ngResource"])
 .controller("TodoCtrl", TodoCtrl)
 .controller("TodoListCtrl", TodoListCtrl)
 .service("TodoRepo", todoRepo)
+.directive("inlineEdit", inlineEditable);
+
 
 // controller for the todo form
 function TodoCtrl(TodoRepo, $scope) {
@@ -57,5 +59,31 @@ function todoRepo($resource) {
     return all || (all = Todo.query());
   }
 
+}
+
+function inlineEditable() {
+  return {
+    transclude: true,
+    template: "<div> <div ng-show='!editing' ng-transclude></div> <form ng-submit='done' ng-show='editing'><input ng-model=target><button>Ok</button></form> </div>",
+    replace: true,
+    scope: {
+      target: "=inlineEdit",
+    },
+    link: function(scope, el, attr) {
+      scope.editing = false;
+
+      el.on("click", function() {
+        scope.$apply(function() {
+          scope.editing = true;
+        });
+      });
+
+      el.on("submit", function() {
+        scope.$apply(function() {
+          scope.editing = false;
+        });
+      });
+    }
+  }
 }
 
