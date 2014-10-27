0da51ab7454cdad915b04f53ae7324379127b99d
Tim Ruffles <oi@truffles.me.uk>
2014-10-27 11:32:12 +0000

    TODOs

--- exercises/controllers-scopes/main.js
+++ exercises/controllers-scopes/main.js
@@ -1,16 +1,16 @@
 var app = angular.module("exercise",[]);
 
-// YOUR CODE
+// TODO
 // we're missing a controller
 // - you'll see an error in the console that'll suggest what to fix.
 
 app.controller("listful",function($scope) {
-  // YOUR CODE
+  // TODO
   $scope.list = []; // looking a bit bare
 })
 
 app.controller("tabs",function($scope) {
-  // YOUR CODE
+  // TODO
   // the view is expecting a function to be defined
   // on the scope - can you fill it in?
 })
