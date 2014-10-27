02d3eae0189914eed703ed33315b5b01a8a7bd2c
Tim Ruffles <oi@truffles.me.uk>
2014-10-27 16:20:19 +0000

    comments

--- exercises/complete-example/main.js
+++ exercises/complete-example/main.js
@@ -3,21 +3,30 @@ angular.module("exercise",["ngResource"])
 .controller("TodoListCtrl", TodoListCtrl)
 .service("TodoRepo", todoRepo)
 
+// controller for the todo form
 function TodoCtrl(TodoRepo, $scope) {
+  // create a single todo from form data
   $scope.create = function() {
     TodoRepo.create($scope.todo);
     $scope.todo = {};
   }
 }
 
+// controller for a list of todos
 function TodoListCtrl(TodoRepo, $scope) {
+  // retrieves the complete list of todos
   $scope.todos = TodoRepo.all();
 }
 
+// todoRepo abstracts the persistence of Todos, in an
+// active-record style.
 function todoRepo($resource) {
 
+  // define our resource
   var Todo = $resource("/api/todo/:id",
+      // take the value for param :id from the id property
       {id: "@id"},
+      // add a 'create' method that posts for creating items
       { 
         create: { method: "POST" },
       });
@@ -26,17 +35,26 @@ function todoRepo($resource) {
 
   this.create = function(data) {
     var todo = Todo.create(data);
-    all.push(todo);
+    // if we already have the cache...
+    if(all) {
+      // update it
+      all.push(todo);
+    } else {
+      // or retrieve it, then cache object
+      all = Todo.query();
+      all.$promise.then(function() {
+        all.push(todo);
+      });
+    }
+    
+    // save our todo to the server
     todo.$save();
     return todo;
   }
 
+  // retrieve list of todos, from local cache if possible
   this.all = function() {
     return all || (all = Todo.query());
-  }
-
-  this.get = function(id) {
-    return Todo.get(id);
   }
 
 }
