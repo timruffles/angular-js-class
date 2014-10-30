27279e9d037088d2e4a3b198359f2764bf74db65
Tim Ruffles <oi@truffles.me.uk>
2014-10-30 09:27:24 +0000

    answer

--- exercises/http/main.js
+++ exercises/http/main.js
@@ -1,4 +1,4 @@
-// TODO - implement orderRepo
+// EXERCISE CODE - implement orderRepo
 
 // orderRepo abstracts the persistence of Orders, in an
 // active-record style.
@@ -9,6 +9,13 @@ function orderRepo($resource) {
   // TODO define our resource
   // - the URL is '/api/order'
   // - add a `create` method that POSTs
+  var Order = $resource("/api/order/:id",
+      // take the value for param :id from the id property
+      {id: "@id"},
+      // add a 'create' method that posts for creating items
+      { 
+        create: { method: "POST" },
+      });
 
   // this is a place to cache the list of
   // all orders, so we can update it with newly created ones
@@ -16,11 +23,27 @@ function orderRepo($resource) {
 
   // TODO retrieve list of orders, from local cache if possible
   repo.all = function() {
+    if(!all) all = Order.query();
+    return all;
   }
 
   // TODO create order, and update cached list,
   // *or* query for all and then update
   repo.create = function(data) {
+    var order = Order.create(data);
+    // if we already have the cache...
+    if(all) {
+      // update it
+      all.push(order);
+    } else {
+      // or retrieve it, then cache object
+      all = Order.query();
+      all.$promise.then(function() {
+        all.push(order);
+      });
+    }
+    
+    return order;
   }
 
 
@@ -28,22 +51,25 @@ function orderRepo($resource) {
 
 }
 
-// END TODO
+// END OF EXERCISE CODE
 
 angular.module("exercise",["ngResource"])
 .controller("orderList",function($scope) {
-  // YOUR CODE
   $scope.done = function(index) {
     var order = $scope.orders[index];
+
+    order.deleting = true;
     order.$delete()
       .then(function() {
         var index = $scope.orders.indexOf(order);
         $scope.orders.splice(index,1);
+      }, function() {
+        order.deleting = false;
+        console.error("Couldn't delete - check the resource URL");
       });
   }
 })
 .controller("addOrder",function($scope, orderRepo) {
-  // YOUR CODE
   $scope.add = function(newOrder) {
     var record = orderRepo.create(newOrder);
     $scope.newOrder = {};
--- exercises/http/repo.js
+++ exercises/http/repo.js
@@ -8,20 +8,37 @@ function orderRepo($resource) {
   var repo = {};
 
   // define our resource
+  var Order = $resource("/api/order/:id",
+      // take the value for param :id from the id property
+      {id: "@id"},
+      // add a 'create' method that posts for creating items
+      { 
+        create: { method: "POST" },
+      });
 
-  // this is where we'll cache the list of
-  // all orders, so we can update it with newly created ones
   var all;
+
+  repo.create = function(data) {
+    var order = Order.create(data);
+    // if we already have the cache...
+    if(all) {
+      // update it
+      all.push(order);
+    } else {
+      // or retrieve it, then cache object
+      all = Order.query();
+      all.$promise.then(function() {
+        all.push(order);
+      });
+    }
+    
+    return order;
+  }
 
   // retrieve list of orders, from local cache if possible
   repo.all = function() {
+    return all || (all = Order.query());
   }
-
-  // create order, and update cached list,
-  // *or* query for all and then update
-  repo.create = function(data) {
-  }
-
 
   return repo;
 
