df3610335911f6a6804a3034995c8f213436cba8
Tim Ruffles <oi@truffles.me.uk>
2014-10-30 09:59:55 +0000

    Revert "answers"
    This reverts commit 67baf364832ac0bb05a8c86b537b3d0fcf028d71.

--- exercises/controllers-scopes/main.js
+++ exercises/controllers-scopes/main.js
@@ -3,23 +3,14 @@ var app = angular.module("exercise",[]);
 // TODO
 // we're missing a controller
 // - you'll see an error in the console that'll suggest what to fix.
-app.controller("defineMe",function($scope) {
-});
 
 app.controller("listful",function($scope) {
   // TODO
-  $scope.list = [
-    {title:"bagle"},
-    {title:"danish"},
-    {title:"muffin"},
-  ]; // looking a bit bare
-});
+  $scope.list = []; // looking a bit bare
+})
 
-app.controller("TabsCtrl",function($scope) {
+app.controller("tabs",function($scope) {
   // TODO
-  this.showTab = function(tab) {
-    $scope.tab = tab;
-  }
-
-  this.showTab("a");
-});
+  // the view is expecting a function to be defined
+  // on the scope - can you fill it in?
+})
--- exercises/first-directive/main.js
+++ exercises/first-directive/main.js
@@ -1,65 +1,7 @@
 var exercise = angular.module("exercise",[]);
 
 exercise.directive("flash",function() {
-  return function(scope, el, attrs) {
-
-    el.on("click", function() {
-      el.addClass("go");
-    });
-
+  return function(scope,el,attrs) {
+    // YOUR CODE
   }
-});
-
-
-// <input ng-model='user.name'
-app.directive("iqModel", function() {
-  return {
-    scope: {
-      model: "=ngModel",
-    },
-    link: function(scope, el, attrs) {
-      el.on("input", function() {
-        scope.model = el.val()
-      });
-    }
-  };
-})
-
-
-// <modal title="Hello {{ user.name }}">
-//   <!-- template content -->
-// </modal>
-app.directive("modal", function() {
-  return {
-    // element = <modal>
-    // *attribute = <div modal>
-    // data = <div data-modal>
-    // class = <div class="modal">
-    restrict: "EADC",
-    template: "<div></div>",
-    templateUrl: "/some/template.html",
-    scope: {
-      title: "@title",
-    },
-    link: function(scope, el, attrs) {
-      el.on("click", function() {
-        // kick off modal...
-        //
-        modalTitleEl.innerHTML = scope.title;
-      });
-    }
-  };
-})
-
-
-// <button iq-click="message(user)"></button>
-app.directive("iqClick", function() {
-  return {
-    scope: {
-      click: "&iqClick",
-    },
-    link: function(scope, el, attrs) {
-      el.on("click", scope.click);
-    }
-  };
 })
