608ca00f3ef4967126f7e9ab48129acf769dd97e
Tim Ruffles <oi@truffles.me.uk>
2015-01-16 11:38:17 +0000

    push-state answer

--- exercises/routes/main.js
+++ exercises/routes/main.js
@@ -3,11 +3,21 @@ var app = angular.module("exercise",["ngRoute"]);
 app.config(function($locationProvider) {
   // won't work for file://
   // normally we'd use this to enable pushState
-  // $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 });
 
 app.config(function($routeProvider) {
   // we'd like to define three routes for the three paths
   // - how can we do that?
+  $routeProvider
+    .when("/routes/baked-goods", {
+      templateUrl: "/routes/baked-goods.html",
+    })
+    .when("/routes", {
+      templateUrl: "/routes/home.html",
+    })
+    .when("/routes/coffee", {
+      templateUrl: "/routes/coffee.html",
+    })
 });
 
