b82acdab2ed0ab8f0742515df9568cd0f4fc0695
Tim Ruffles <oi@truffles.me.uk>
2015-01-16 10:56:16 +0000

    crud mvc answer with inline-edit

--- exercises/crud-mvc/main.js
+++ exercises/crud-mvc/main.js
@@ -1,11 +1,36 @@
 var app = angular.module("exercise",[]);
 
 app.controller("orderList",function($scope) {
-  // YOUR CODE
+  $scope.remove = function(order) {
+    var index = $scope.orders.indexOf(order);
+    $scope.orders.splice(index, 1);
+    console.log($scope.orders);
+  }
+
+  $scope.edit = function(order) {
+    order.editing = true;
+  }
 });
+
+app.directive("fancyValidation",function() {
+  return {
+    require: "ngModel", 
+    link: function(scope, el, attrs, ngModelController) {
+      ngModelController.$validators.fancy = function(value) {
+        return value && value.length > 5;
+      }
+    }
+  }
+});
+
+
 
 app.controller("addOrder",function($scope) {
   // YOUR CODE
+  $scope.createOrder = function() {
+    $scope.orders.push($scope.order);
+    $scope.order = {};
+  }
 });
 
 app.controller("orders",function($scope) {
