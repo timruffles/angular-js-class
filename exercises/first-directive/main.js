d8ea7a9e1bec8de9584e353767b567b214ff41b9
Tim Ruffles <oi@truffles.me.uk>
2015-07-16 15:46:42 +0100

    directives

--- exercises/first-directive/main.js
+++ exercises/first-directive/main.js
@@ -1,7 +1,41 @@
 var exercise = angular.module("exercise",[]);
 
 exercise.directive("flash",function() {
-  return function(scope,el,attrs) {
-    // YOUR CODE
+  return {
+    restrict: "E",
+    scope: {
+      message: "@",
+    },
+    template: 
+      "<p class='alert-box success'>" +
+      "  {{ message }} " + 
+      "</p>",
+    link: function(scope, el,attrs) {
+      scope.message =
+        scope.message || "Here's a temporary message - click to dismiss";
+
+      var rawEl = el[0];
+      // YOUR CODE
+      el.on("click", function() {
+        el.remove(); 
+      });
+    }
+  }
+})
+
+
+exercise.directive("highlight",function() {
+  return {
+    restrict: "A",
+    link: function(scope, el,attrs) {
+
+      el.on("mouseover", function() {
+        el.find("p").css("background", "yellow"); 
+      })
+
+      el.on("mouseout", function() {
+        el.find("p").css("background", null); 
+      })
+    }
   }
 })
