193097a528ff9af6fda2f4e2b6955d945ec563c9
Tim Ruffles <oi@truffles.me.uk>
2015-07-16 14:19:49 +0100

    Crud mvc one

--- exercises/crud-mvc/main.js
+++ exercises/crud-mvc/main.js
@@ -4,16 +4,50 @@ app.controller("orderList",function($scope) {
   // TODO implement deleting done orders
 });
 
-app.controller("addOrder",function($scope) {
-  // TODO implement new orders
+app.controller("addOrder",function(
+  $scope
+  , Order
+) {
+
+  $scope.add = function() {
+    var newOrder = new Order({
+      text: $scope.order.text,
+    });
+
+    var dupe = $scope.orders.some(function(order) {
+      return order.eq(newOrder); 
+    });
+
+    if(dupe) {
+      return;
+    }
+
+    $scope.orders.push(newOrder);
+    $scope.order = {};
+  };
 });
 
-app.controller("orders",function($scope) {
+app.controller("orders",function($scope, Order) {
   $scope.orders = [
-    {text:"warm cheese and mushroom cupcake"},
-    {text:"citrus banoffee tart"},
-    {text:"polenta meringue"},
-    {text:"half a baker's dozen of unboiled bagles"}
+    new Order({text:"warm cheese and mushroom cupcake"}),
+    new Order({text:"citrus banoffee tart"}),
+    new Order({text:"polenta meringue"}),
+    new Order({text:"half a baker's dozen of unboiled bagles"}),
   ];
 })
 
+app.factory("Order", function() {
+
+  function Order(data) {
+    this.text = data.text;
+  }
+
+  Order.prototype = {
+    eq: function(otherOrder) {
+      return otherOrder.text === this.text; 
+    }
+  }
+
+  return Order;
+  
+});
