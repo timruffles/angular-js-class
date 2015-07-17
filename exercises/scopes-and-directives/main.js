5d5e4d505a1149e8903340a4d0d427100b81d5e7
Tim Ruffles <oi@truffles.me.uk>
2015-07-17 12:28:50 +0100

    step 1

--- exercises/scopes-and-directives/main.js
+++ exercises/scopes-and-directives/main.js
@@ -17,38 +17,66 @@ angular.module("exercise",[])
   // defaults to only matching on attributes
   return {
 
-    // TODO configure as custom element
-    // TODO configure scope access for API
-    // TODO configure to transclude dynamic content
-
+    // this is a custom tag - e.g <tr-model, not <div tr-model
+    restrict: "E",
 
     templateUrl: "./modal.html",
     // replace true as we can't compose with directives
     // on our element
     replace: true,
 
+    // configure to transclude dynamic content
+    transclude: true,
+
+    // =, @, &
+    scope: {
+      // configure scope access for API
+      title: "@",
+      options: "&",
+      acceptedModel: "=",
+      dismissed: "&",
+      shown: "&",
+    },
 
     link: function(scope, el, attrs) {
 
       var config = {};
+      var hidden = false;
+
+      var rawEl = el[0];
+      var parent = rawEl.parentElement;
 
       hide();
 
-      // TODO need to watch options (as object) and update config
+      // watch options (as object) and update config
+      scope.$watchCollection(scope.options, 
+      function(nv, ov) {
+        config = nv;
+      });
 
-
-      // TODO shown and hide modal when 'shown'
-      // value changes
-
+      scope.$watch(scope.shown, function(shown) {
+        // show and hide modal when 'shown'
+        shown ? show() : hide(); 
+      });
 
       // WARNING - angular.element().remove()
       // will destroy the scope, not just pluck
       // element from DOM. Not what we want
       function show() {
+        if(!hidden) {
+          return;
+        }
         // TODO ensure element is in DOM
+        parent.appendChild(rawEl);
+        hidden = false;
       }
+
       function hide() {
-        // TODO remove element from DOM
+        if(hidden) {
+          return;
+        }
+        parent.removeChild(rawEl);
+        hidden = true;
       }
 
       function config(conf, old) {
